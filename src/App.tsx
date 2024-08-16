8aa0a55fc67864cca847e9aeee1c2e6f1e6b21be
Klas Mellbourn <klas@mellbourn.net>
2024-08-16 13:04:05 +0200

    add color mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  Heading,
+  VStack,
+  Button,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import Board from "./components/Board";
 
 const App: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue("gray.50", "gray.800");
+  const color = useColorModeValue("gray.800", "gray.50");
+
   return (
-    <Box textAlign="center" fontSize="xl" p={3}>
-      <VStack spacing={5}>
-        <Heading as="h1" size="2xl">
-          Tic-Tac-Toe
-        </Heading>
+    <Box
+      textAlign="center"
+      fontSize="xl"
+      p={3}
+      bg={bgColor}
+      color={color}
+      minH="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <VStack spacing={5} w="full" maxW="md">
+        <Box
+          w="full"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading as="h1" size="2xl">
+            Tic-Tac-Toe
+          </Heading>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          </Button>
+        </Box>
         <Board />
       </VStack>
     </Box>
